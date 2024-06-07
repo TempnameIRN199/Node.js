1a541aeda00b7bcc248c90ea4d70a13957f62ebe
TempnameIRN199 <vityayuzbekov602@gmail.com>
2024-06-08 01:44:36 +0300

    fix first home work

--- first_hw/app.js
+++ first_hw/app.js
@@ -1,19 +1,15 @@
 const http = require('http');
-const url = new URL('http://localhost:8080/default.htm?year=2017&month=february');
+const url = require('url');
 
 http.createServer((require, response) => {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<h1>URL</h1>');
-    response.write(`<p>Host: ${url.host}</p>`);
-    response.write(`<p>Path: ${url.pathname}</p>`);
-    response.write('<p>Params:</p>');
-    response.write('<ul>');
-    for (let [key, value] of url.searchParams) {
-        response.write(`<li>${key}: ${value}</li>`);
-    }
-    response.write('</ul>');
-    response.end();
+    const parsedUrl = url.parse(require.url, true);
+    const host = require.headers.host;
+    const pathname = parsedUrl.pathname;
+    const query = parsedUrl.query;
+    const responseMessage = `Host: ${host}, Pathname: ${pathname}, Query: ${JSON.stringify(query)}`;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end(responseMessage);
 }
-).listen(3000, 'localhost', function() {
+).listen(3000, '127.0.0.1', function() {
     console.log('Engine start');
 });
